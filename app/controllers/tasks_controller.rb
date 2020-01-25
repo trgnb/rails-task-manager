50cb82c9e3363dff56111e7618a6fdf8d59c7da5
Belis Turegun <trgnb6@gmail.com>
2020-01-25 16:43:22 -0500

    horrible ui but done

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,35 @@
 class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(params.require(:task).permit(:title, :details, :completed))
+    @task.save
+    redirect_to(tasks_path)
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(params.require(:task).permit(:title, :details, :completed))
+    redirect_to(task_path)
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to(tasks_path)
+  end
 end
